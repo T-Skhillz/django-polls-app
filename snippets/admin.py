9f69aa6ccdd53aa30eae20d99f766b331eab6deb
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2026-01-08 08:35:51 +0100

    Stop tracking snippets

--- snippets/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from snippets.models import Snippet
-
-class SnippetAdmin(admin.ModelAdmin):
-    list_display = ("title", "language","style" , "linenos", "created_at")
-    list_filter = ["language", "style", "created_at"]
-    search_fields = ["language", "title", "created_at"]
-
-admin.site.register(Snippet, SnippetAdmin)
--- snippets/serializers.py
+++
@@ -1,8 +0,0 @@
-from snippets.models import Snippet
-from rest_framework import serializers
-
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Snippet
-        fields = ["id", "title", "code", "language", "style", "linenos"]
-
